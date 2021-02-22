264401630e66188728e9da23b4210be01eb2df04
Imre Mathias <imremathias@outlook.com>
2021-02-22 14:54:36 +0100

    FEAT: SHAPE CLASS
    Add fields and methods:
     - Import color class
     - Empty constructor
     - Constructor with arguments
     - Getters and setters
     - toString override method

--- src/Shape.java
+++ src/Shape.java
@@ -1,2 +1,40 @@
+import java.awt.*;
+
 public class Shape {
+    protected Color color;
+    protected boolean filled;
+
+    public void Shape() {
+        this.color = Color.red;
+        this.filled = true;
+    }
+
+    public void Shape(Color color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color=" + color +
+                ", filled=" + filled +
+                '}';
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
 }

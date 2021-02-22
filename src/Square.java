659a2df6b19ecc2ad02e1cd4dd2ba0bdfd1c575d
Imre Mathias <imremathias@outlook.com>
2021-02-22 16:19:23 +0100

    FEAT: SQUARE-CLASS
    Add Square-class which extends from Shape-class:
    - Inheritance
    - Geometry-fields
    - Constructors
    - toString-method override
    - Geometry-methods associated with squares
    - Getters and setters

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,46 @@
-public class Square {
+import java.awt.*;
+
+public class Square extends Shape {
+    
+    private double side;
+
+    public Square() {
+        super(Color.green, true);
+        side = 1;
+    }
+
+    public Square(double side) {
+        super(Color.green, true);
+        this.side = side;
+    }
+
+    public Square(double side, Color color, boolean filled) {
+        super(color, filled);
+        this.side = side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", color=" + color +
+                ", filled=" + filled +
+                '}';
+    }
+
+    public double getArea() {
+        return side*side;
+    }
+
+    public double getPerimeter() {
+        return side*4;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
 }

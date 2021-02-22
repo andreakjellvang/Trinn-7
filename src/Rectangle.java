84967ff7952433c45bd5540f64b17d434c787d05
Imre Mathias <imremathias@outlook.com>
2021-02-22 16:07:26 +0100

    FEAT: RECTANGLE CLASS
    Add Rectangle-class which extends from Shape-class:
    - Inheritance
    - Geometry-fields
    - Constructors
    - toString-method override
    - Geometry-methods associated with rectangles
    - Getters and setters

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,59 @@
-public class Rectangle {
+import java.awt.*;
+
+public class Rectangle extends Shape {
+
+    private double width;
+    private double length;
+
+    public Rectangle() {
+        super(Color.green, true);
+        width = 1;
+        length = 2;
+    }
+
+    public Rectangle(double width, double length) {
+        super(Color.green, true);
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(double width, double length, Color color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                ", color=" + color +
+                ", filled=" + filled +
+                '}';
+    }
+
+    public double getArea() {
+        return width*length;
+    }
+
+    public double getPerimeter() {
+        return 2*(width+length);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
 }
